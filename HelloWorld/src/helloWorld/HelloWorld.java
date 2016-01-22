e9a429ecf825f7df274976799a3c32f19c6c6b1d
Igor Burlak <burlakisgreat@gmail.com>
2016-01-22 16:27:32 -0600

    Hello World
    Added hello world project

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Prints to console a message
+		System.out.println("Hello Beautiful World");
+
+	}
+
+}
